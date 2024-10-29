fe34155e6a3cfcdb3d9f386055455dc156fdd2fb
Christophe Carbin <blakehziir@gmail.com>
2024-10-29 12:35:08 +0100

    dockerfile

--- orq_api_auth/settings.py
+++ orq_api_auth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-948)b_lkizmg=(5mdh6h$+x27x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'  # Convertir en booléen
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1']
 
