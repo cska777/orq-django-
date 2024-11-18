b2c9937263d813e59d3c8b14eb2de1d9c4a6e78f
Christophe Carbin <blakehziir@gmail.com>
2024-11-18 11:44:25 +0100

    Configure for Railway deployment

--- orq_api_auth/settings.py
+++ orq_api_auth/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
@@ -59,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
