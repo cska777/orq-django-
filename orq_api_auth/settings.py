9d3996122887f4cb01424757a8da53925e70d6db
Christophe Carbin <blakehziir@gmail.com>
2024-11-18 11:39:21 +0100

    Configure for Railway deployment

--- orq_api_auth/settings.py
+++ orq_api_auth/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
@@ -139,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
